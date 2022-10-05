1d6542366ebe31322a906d48daf3aaea675d4e7a
anaineskessler <anaineskessler@gmail.com>
2022-10-04 21:10:00 -0300

    actualizado fetch

--- js/tragos.js
+++ js/tragos.js
@@ -1,24 +1,24 @@
 let recetasInternac = document.getElementById("recetasInternac");
 
-
 const listaTragos = async () => {
 try {
     let response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
-    cocktail = await response.json();
-    let tragosElegidos = cocktail.results;
-    console.log("HOLA");
-    console.log(cocktail);
-    console.log("HOLA2");
+    let data = await response.json();
+    let tragosElegidos = data.drinks;
+
+    console.log(tragosElegidos);
+    console.log(data);
     
-tragosElegidos.forEach(tragos => {
-        console.log("hola");
+    tragosElegidos.forEach(tragos => {
         const div = document.createElement("div");
 
         div.innerHTML = `
-        <h1>Código: ${tragos.idDrink}</h2>
-        <p>Categoría: ${tragos.strCategory}</p>        
-        <p>Nombre: ${tragos.strDrink}</p>        
-        <img src"${tragos.strDrinkThumb}">
+        <h3 class= "color_marron_sm">Código: ${tragos.idDrink}</h3>
+        <p><b>Categoría:</b> ${tragos.strCategory}</p>        
+        <p><b>Nombre:</b> ${tragos.strDrink}</p>    
+        <img class= "imagenTragos" src="${tragos.strDrinkThumb}">
+        <p><b>Ingredientes:</b> ${tragos.strIngredient1}, ${tragos.strIngredient2}, ${tragos.strIngredient3}, ${tragos.strIngredient4}, ${tragos.strIngredient5}, ${tragos.strIngredient6}, ${tragos.strIngredient7} </p>
+        <br>
         <hr/>
         `;
         recetasInternac.append(div);
@@ -27,8 +27,9 @@ tragosElegidos.forEach(tragos => {
         
         let div =document.createElement("div");
         div.innerHTML=`
-        <h1>Próximamente!</h1>`;
+        <h1>Ha ocurrido un error, por favor intentar luego!</h1>`;
         recetasInternac.append(div);
     }
 };
-listaTragos()
+
+listaTragos();
